13f77ec7478f05ed8cbd0e31c83b3486441d4a57
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-06-30 16:15:32 +0800

    Add statistics to the dashboard.fxml

--- src/main/java/com/example/dsafinals/controllers/DashboardController.java
+++ src/main/java/com/example/dsafinals/controllers/DashboardController.java
@@ -1,4 +1,30 @@
 package com.example.dsafinals.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
+import org.kordamp.ikonli.javafx.FontIcon;
+
 public class DashboardController {
+    @FXML
+    private StackPane entriesIconBox;
+
+    @FXML
+    private StackPane photosIconBox;
+
+    @FXML
+    private StackPane albumsIconBox;
+
+    @FXML
+    public void initialize() {
+        entriesIconBox.getChildren().add(createIcon("mdi2b-book-open-page-variant"));
+        photosIconBox.getChildren().add(createIcon("mdi2i-image"));
+        albumsIconBox.getChildren().add(createIcon("mdi2f-folder"));
+    }
+
+    private FontIcon createIcon(String iconCode) {
+        FontIcon icon = new FontIcon(iconCode);
+        icon.setIconSize(24);
+        icon.getStyleClass().add("sidebar-icon");
+        return icon;
+    }
 }
